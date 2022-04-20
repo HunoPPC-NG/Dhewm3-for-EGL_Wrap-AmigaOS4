cd6d813847d1b5b1d20f2fe2474671cbdfdab10b
Hugues Nouvel <70510644+HunoPPC-NG@users.noreply.github.com>
2022-04-20 10:35:24 +0200

    Add files via upload

--- neo/config.h
+++ neo/config.h
@@ -1,1 +1,18 @@
+#ifndef __DOOM3_CONFIG_H__
+#define __DOOM3_CONFIG_H__
 
+#define BUILD_OS				"amigaos4"
+#define BUILD_CPU				"ppc"
+#define D3_OSTYPE				"amigaos4"
+#define D3_ARCH					"ppc"
+
+#define BUILD_LIBRARY_SUFFIX	".so"
+
+#define BUILD_LIBDIR			"base"
+#define BUILD_DATADIR			"base"
+
+//#define HAVE_JPEG_MEM_SRC
+
+//#define ID_ENABLE_CURL
+
+#endif
